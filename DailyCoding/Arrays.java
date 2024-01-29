4132ca308b2ada6d5f38c92fbec1403a85c75b16
Steven W <134321560+swlincoln0519@users.noreply.github.com>
2024-01-29 16:41:39 +0000

    moved ConcatArrays to Array.java folder

--- DailyCoding/Arrays.java
+++ DailyCoding/Arrays.java
@@ -1,10 +1,14 @@
 public class Arrays {
     public static void main(String[] args) {
+        //ArrayAscending
         System.out.println(Arrays.toString(arrAscending(2)));
         System.out.println(Arrays.toString(arrAscending(5)));
         System.out.println(Arrays.toString(arrAscending(7)));
         System.out.println(Arrays.toString(arrAscending(9)));
         
+        //ConcatArrays
+        String [] words = {"popcorn", "ice cream", "licorice", "cotton candy"};
+        System.out.println(concatArr(words)); // returns popcorn, ice cream, licorice, and cotton candy.
     }
 
     public static arrayAscend(){
@@ -13,5 +17,22 @@ public class Arrays {
             result[i] = i;
         }
         return result;
+    }
+
+    public static String concatArr(String [] words) {
+        if (words.length == 0) {
+            return "";
+        }
+        else if (words.length == 1) {
+            return words[0];
+        }
+        else {
+            StringBuilder result = new StringBuilder();
+        for (int i = 0; i < words.length - 1; i++) {
+            result.append(words[i]).append(", ");
+        }
+        result.append("and ").append(words[words.length - 1]);
+        return result.toString();
+        }
     }
 }
