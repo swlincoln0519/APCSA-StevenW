d3783b7dbf42665b5b9ac8f3ad9b7f237c528c97
Steven W <134321560+swlincoln0519@users.noreply.github.com>
2024-02-01 19:58:32 +0000

    moved InsertIntoArray to DailyArrays folder deleted InsertIntoArray folder

--- DailyCoding/DailyArrays.java
+++ DailyCoding/DailyArrays.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class DailyArrays {
     public static void main(String[] args) {
-        // ArrayAscending
+        //ArrayAscending
         System.out.println(Arrays.toString(arrayAscend(2)));
         System.out.println(Arrays.toString(arrayAscend(5)));
         System.out.println(Arrays.toString(arrayAscend(7)));
@@ -36,7 +36,16 @@ public class DailyArrays {
                 System.out.print(", ");
             }
         }
+
+        //InsertIntoArray
+        String[] teachers = {"Mr. Luu", "Mr. Garcia", "Mr. Brewer", "Ms. Situ", "Ms. Willis", "Ms. Crabtree"};
+        teachers = insertIntoArray(teachers, 2, "Mr. Skramstad");
+        for (int i = 0; i < teachers.length; i++) {
+            String teacher = teachers[i];
+            System.out.print(teacher + " | ");
+        }
     }
+
 
     public static int[] arrayAscend(int num){
         int[] result = new int[num + 1];
@@ -89,6 +98,18 @@ public class DailyArrays {
         int[] newArr = new int[sequence.length * n];
         for (int i = 0; i < newArr.length; i++) {
             newArr[i] = sequence[i % sequence.length];
+        }
+        return newArr;
+    }
+
+    public static String[] insertIntoArray(String[] arr, int index, String name) {
+        String[] newArr = new String[arr.length + 1];
+        for (int i = 0; i < index; i++) {
+            newArr[i] = arr[i];
+        }
+        newArr[index] = name;
+        for (int i = index + 1; i < newArr.length; i++) {
+            newArr[i] = arr[i - 1];
         }
         return newArr;
     }
