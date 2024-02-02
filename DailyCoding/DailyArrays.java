b4340fd50de7bb6aac30fae582486e79198791dd
Steven W <134321560+swlincoln0519@users.noreply.github.com>
2024-02-02 16:39:05 +0000

    moved the PasswordGenerator to Daily Arrays folder deleted PasswordGenerator folder

--- DailyCoding/DailyArrays.java
+++ DailyCoding/DailyArrays.java
@@ -12,11 +12,9 @@ public class DailyArrays {
         String[] words = {"popcorn", "ice cream", "licorice", "cotton candy"};
         System.out.println(concatArr(words));
 
-        // ConcatArray
+        //ConcatArrays
         int[] arr1 = {1, 2, 3, 4, 5};
         int[] arr2 = {6, 7, 8, 9, 10};
-        
-        //ConcatArrays
         int[] combinedArray = combineArrays(arr1, arr2);
         for (int num : combinedArray) {
             System.out.print(num + " ");
@@ -51,9 +49,17 @@ public class DailyArrays {
         for (int i = 0; i < randArr.length; i++) {
             System.out.print(randArr[i] + " ");
         }
+
+        //RandomPasswords
+        int num = (int) (Math.random() * 10) + 1;
+        String[] randPass = randomVal(num);
+        for (int i = 0; i < num; i++) {
+            System.out.println(randPass[i]);
+            System.out.println();
+        }
     }
 
-
+    //ArrayAscending
     public static int[] arrayAscend(int num){
         int[] result = new int[num + 1];
         for (int i = 0; i <= num; i++) {
@@ -62,6 +68,7 @@ public class DailyArrays {
         return result;
     }
 
+    //ConcatArray
     public static String concatArr(String[] words) {
         if (words.length == 0) {
             return "";
@@ -77,6 +84,7 @@ public class DailyArrays {
         }
     }
 
+    //ConcatArrays
     public static int[] concatArrays(int[] arr1, int[] arr2) {
         int[] concatArray = new int[arr1.length + arr2.length];
         System.arraycopy(arr1, 0, concatArray, 0, arr1.length);
@@ -84,6 +92,7 @@ public class DailyArrays {
         return concatArray;
     }
 
+    //ChangeArrByFive
     public static void changebyFive(double[] arrayQuintuple) {
         for (int i = 0; i < arrayQuintuple.length; i++) {
             if (arrayQuintuple[i] > 0) {
@@ -101,6 +110,7 @@ public class DailyArrays {
         }
     }
 
+    //CloneArray
     public static int[] cloneArray(int[] sequence, int n) {
         int[] newArr = new int[sequence.length * n];
         for (int i = 0; i < newArr.length; i++) {
@@ -109,6 +119,7 @@ public class DailyArrays {
         return newArr;
     }
 
+    //InsertIntoArray
     public static String[] insertIntoArray(String[] arr, int index, String name) {
         String[] newArr = new String[arr.length + 1];
         for (int i = 0; i < index; i++) {
@@ -121,11 +132,49 @@ public class DailyArrays {
         return newArr;
     }
 
+    //RandomValues
     public static int[] randomValue(int n) {
         int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
             arr[i] = (int) (Math.random() * 100) + 1;
         }
         return arr;
+    }
+
+    //RandomPasswords
+    public static String[] randomVal(int num) {
+        String[] arr = new String[num];
+        for (int i = 0; i < num; i++) {
+            arr[i] = generateCustomPassword(2, 2, 2, 10);
+        }
+        return arr;
+    }
+    public static String generateCustomPassword(int minSymbols, int minUpperCase, int minLowerCase, int length) {       
+    String upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    String lowerCase = "abcdefghijklmnopqrstuvwxyz";
+    String numbers = "0123456789";
+    String symbols = "!@#$%&*_+=?";
+    String password = "";
+    
+    for (int i = 0; i < minUpperCase; i++) {
+    int randUpper = (int) Math.floor(Math.random() * upperCase.length());
+    password += upperCase.substring(randUpper, randUpper + 1);
+    }
+    for (int i = 0; i < minLowerCase; i ++) {
+    int randLower = (int) Math.floor(Math.random() * lowerCase.length());
+    password += lowerCase.substring(randLower, randLower + 1);
+    }
+    for (int i = 0; i < minSymbols; i ++) {
+    int randSym = (int) Math.floor(Math.random() * 10);
+    password += symbols.substring(randSym, randSym + 1);
+    }
+    int lengthy = length - minUpperCase - minLowerCase - minSymbols;
+    
+    for (int i = 0; i < lengthy; i ++) {
+    int randNum = (int) Math.floor(Math.random() * 10);
+    password += numbers.substring(randNum, randNum + 1);
+    }
+    
+    return password;
     }
 }
