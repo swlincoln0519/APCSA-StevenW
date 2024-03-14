0e06ad3bbeca1339a81d70228b50e315765765a3
Steven W <134321560+swlincoln0519@users.noreply.github.com>
2024-03-14 21:17:29 +0000

    added zeroToMax from codehs (dailycode) deleted getMinMax and reorderArr and mvoed it to DailyCoding folder

--- DailyCoding/Semester2.java
+++ DailyCoding/Semester2.java
@@ -34,6 +34,26 @@ public class Semester2 {
         System.out.println(longestWordLen("What is the longest word in this sentence."));
         System.out.println(longestWordLen("What you most thankful for?"));
         System.out.println(longestWordLen("Let's go Tigers!"));
+
+        //reorderArr
+        int[] arr1 = {1, 0, 0, 1};
+        int[] result = reorderArr(arr1);
+        
+        for (int num : result) {
+            System.out.print(num + " ");
+        }
+
+        //zeroToMax
+        int[] array1 = {0, 5, 0, 3};
+        int[] array2 = {0, 4, 0, 3};
+        
+        System.out.println(Arrays.toString(zeroToMax(arr1)));
+        System.out.println(Arrays.toString(zeroToMax(arr2)));
+        
+        //getMinMax
+        double[] array3 = {2.3, 2.1, 5.4, 8.9, 7.7, 1.0, .95, 9.9, 2.2};
+        double[] results = minMax(arr1);
+        System.out.println("Minimum: " + results[0] + ", " + "Maximum: " + results[1]);
     }
 
     public static int sumInt(int n1, int n2) {
@@ -87,5 +107,58 @@ public class Semester2 {
             }
         }
         return max;
+    }
+
+    public static int[] reorderArr(int[] arr) {
+        int countZero = 0;
+        int[] newArr = new int[arr.length];
+        
+        for (int i =0; i < arr.length; i++) {
+            if (arr[i] == 0) {
+                countZero++;
+            }
+        }
+        
+        for (int i = 0; i < countZero; i++) {
+            newArr[i] = 0;
+        }
+        
+        for (int i = 0, j = countZero; i < arr.length; i++) {
+            if (arr[i] != 0) {
+                newArr[j] = arr[i];
+                j++;
+            }
+        }
+        return newArr; 
+    }
+
+    public static int[] zeroToMax(int[] arr) {
+        int max = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 0) {
+                for (int j = i + 1; j < arr.length; j++) {
+                    if (arr[j] % 2 != 0) {
+                    max = Math.max(max, arr[j]);
+                    }
+                }
+                arr[i] = max;
+            }
+        }
+        return arr;
+    }
+
+    public static double[] minMax(double[] arr) {
+        double max = arr[0]; 
+        double min = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+            else if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        return new double[] {min, max};
     }
 }
